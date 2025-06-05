5e092bd663abd1bfe7637dc9a4b63ea2015cc0ce
Hoàng Trang <success010280@gmail.com>
2025-06-05 17:18:44 +0700

    setup config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography, Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+     <Typography variant="h1" color="secondary">Tasko</Typography>
+     <Button variant="contained">Primary Button</Button>
+    </div>
+  )
+}
+
+export default App;
