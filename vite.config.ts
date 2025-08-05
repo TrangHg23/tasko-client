30da41e202ad5d06a4150a4dc9f845f70f83302a
Hoàng Trang <trang010280@gmail.com>
2025-08-05 15:48:39 +0700

    chore: config manualChunks and add visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    visualizer({
+      open: true, // Tự mở trình duyệt sau khi build
+      gzipSize: true, // Hiển thị kích thước gzip
+      brotliSize: true, // Hiển thị kích thước brotli
+      filename: "dist/stats.html", // File output
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@mui/icons-material'],
-          router: ['react-router'],
-          forms: ['react-hook-form'],
-          animation: ['framer-motion'],
+          vendor: ["react", "react-dom"],
+          mui: ["@mui/material", "@mui/icons-material"],
+          router: ["react-router"],
+          forms: ["react-hook-form"],
+          animation: ["framer-motion"],
+          tanstack: ["@tanstack/react-query"],
+          notistack: ["notistack"],
+          axios: ["axios"],
+          dateFns: ["date-fns"],
         },
       },
     },
@@ -20,7 +34,7 @@ export default defineConfig({
   },
   server: {
     hmr: {
-      overlay: false
-    }
-  }
+      overlay: false,
+    },
+  },
 });
